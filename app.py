0413efed968dfec152d6d43fb6e5bec0a7d1f95a
Piyushcodz <yourlovelyps@gmail.com>
2025-02-14 19:39:13 +0530

    Fixed Errors

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-# pip install pyaudio
 
-import pyttsx3 #pip install pyttsx3
-import speech_recognition as sr #pip install speechRecognition
+
+import pyttsx3 
+import speech_recognition as sr 
 import datetime
-import wikipedia #pip install wikipedia
+import wikipedia 
 import webbrowser
-import os
-import smtplib
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-# print(voices[1].id)
 engine.setProperty('voice', voices[0].id)
 
 
@@ -33,7 +30,6 @@ def wishMe():
     speak("I am Piyush Sharma, How my help you")       
 
 def takeCommand():
-    #It takes microphone input from the user and returns string output
 
     r = sr.Recognizer()
     with sr.Microphone() as source:
@@ -52,21 +48,12 @@ def takeCommand():
         return "None"
     return query
 
-def sendEmail(to, content):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login('youremail@gmail.com', 'your-password')
-    server.sendmail('youremail@gmail.com', to, content)
-    server.close()
-
 if __name__ == "__main__":
     wishMe()
     while True:
-    # if 1:
         query = takeCommand().lower()
 
-        # Logic for executing tasks based on query
+        #Logics
         if 'wikipedia' in query:
             speak('Searching Wikipedia...')
             query = query.replace("wikipedia", "")
@@ -85,29 +72,11 @@ if __name__ == "__main__":
             webbrowser.open("stackoverflow.com")   
 
 
-        elif 'play music' in query:
-            music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
-            songs = os.listdir(music_dir)
-            print(songs)    
-            os.startfile(os.path.join(music_dir, songs[0]))
+        elif 'open spotify' in query:
+            webbrowser.open("https://open.spotify.com")
 
-        elif 'the time' in query:
+        elif 'What is the time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")    
-            speak(f"Sir, the time is {strTime}")
-
-        elif 'open code' in query:
-            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
-            os.startfile(codePath)
-
-        elif 'email to harry' in query:
-            try:
-                speak("What should I say?")
-                content = takeCommand()
-                to = "harryyourEmail@gmail.com"    
-                sendEmail(to, content)
-                speak("Email has been sent!")
-            except Exception as e:
-                print(e)
-                speak("Sorry my friend harry bhai. I am not able to send this email")    
+            speak(f"Sir, the time is {strTime}")  
         else:
             print("No query matched")
